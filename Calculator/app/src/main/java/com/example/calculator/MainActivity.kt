33c11644c1cce740174087e8e98cc6b56ea05c76
L-Mosca <lucas.mosca@madeinweb.app>
2023-01-11 19:07:03 -0300

    Add buttons actions programmatically.

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,109 +14,72 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        // Adiciona botões no código
 
-        val button0 = findViewById<Button>(R.id.btNumber0)
-        val textView = findViewById<TextView>(R.id.tvNumber)
+        // Botões numéricos
+        val bt0 = findViewById<AppCompatButton>(R.id.btNumber0)
+        val bt1 = findViewById<AppCompatButton>(R.id.btNumber1)
+        val bt2 = findViewById<AppCompatButton>(R.id.btNumber2)
+        val bt3 = findViewById<AppCompatButton>(R.id.btNumber3)
+        val bt4 = findViewById<AppCompatButton>(R.id.btNumber4)
+        val bt5 = findViewById<AppCompatButton>(R.id.btNumber5)
+        val bt6 = findViewById<AppCompatButton>(R.id.btNumber6)
+        val bt7 = findViewById<AppCompatButton>(R.id.btNumber7)
+        val bt8 = findViewById<AppCompatButton>(R.id.btNumber8)
+        val bt9 = findViewById<AppCompatButton>(R.id.btNumber9)
 
-        button0.setOnClickListener {
-            textView.text = textView.text.toString().plus("0")
+        // Botões de ação
+        val btAC = findViewById<AppCompatButton>(R.id.btAc)
+        val btX = findViewById<AppCompatButton>(R.id.btErase)
+        val btModule = findViewById<AppCompatButton>(R.id.btModule)
+        val btDivision = findViewById<AppCompatButton>(R.id.btDivision)
+        val btMultiply = findViewById<AppCompatButton>(R.id.btMultiply)
+        val btMinus = findViewById<AppCompatButton>(R.id.btMinus)
+        val btPlus = findViewById<AppCompatButton>(R.id.btPlus)
+        val btEqual = findViewById<AppCompatButton>(R.id.btEqual)
+        val btComma = findViewById<AppCompatButton>(R.id.btComma)
+        val btDot = findViewById<AppCompatButton>(R.id.btPoint)
+
+
+        // Adiciona textView no código
+        val tvCalculator = findViewById<AppCompatTextView>(R.id.tvNumber)
+
+        bt0.setOnClickListener { tvCalculator.text = tvCalculator.text.toString().plus("0") }
+        bt1.setOnClickListener { tvCalculator.text = tvCalculator.text.toString().plus("1") }
+        bt2.setOnClickListener { tvCalculator.text = tvCalculator.text.toString().plus("2") }
+        bt3.setOnClickListener { tvCalculator.text = tvCalculator.text.toString().plus("3") }
+        bt4.setOnClickListener { tvCalculator.text = tvCalculator.text.toString().plus("4") }
+        bt5.setOnClickListener { tvCalculator.text = tvCalculator.text.toString().plus("5") }
+        bt6.setOnClickListener { tvCalculator.text = tvCalculator.text.toString().plus("6") }
+        bt7.setOnClickListener { tvCalculator.text = tvCalculator.text.toString().plus("7") }
+        bt8.setOnClickListener { tvCalculator.text = tvCalculator.text.toString().plus("8") }
+        bt9.setOnClickListener { tvCalculator.text = tvCalculator.text.toString().plus("9") }
+        btAC.setOnClickListener { tvCalculator.text = "" }
+        btX.setOnClickListener {
+            val tvString = tvCalculator.text.toString()
+            if (tvString.isNotEmpty()) {
+                tvCalculator.text = tvString.substring(0, tvString.length - 1)
+            }
         }
+        btDot.setOnClickListener {
+            val tvString = tvCalculator.text.toString()
 
-        val button1 = findViewById<Button>(R.id.btNumber1)
-        val textView = findViewById<TextView>(R.id.tvNumber)
-
-        button1.setOnClickListener {
-            textView.text = textView.text.toString().plus("1")
-        }
-
-        val button2 = findViewById<Button>(R.id.btNumber2)
-        val textView = findViewById<TextView>(R.id.tvNumber)
-
-        button2.setOnClickListener {
-            textView.text = textView.text.toString().plus("2")
-        }
-
-        val button3 = findViewById<Button>(R.id.btNumber3)
-        val textView = findViewById<TextView>(R.id.tvNumber)
-
-        button3.setOnClickListener {
-            textView.text = textView.text.toString().plus("3")
-        }
-
-        val button = findViewById<Button>(R.id.btNumber4)
-        val textView = findViewById<TextView>(R.id.tvNumber)
-
-        button4.setOnClickListener {
-            textView.text = textView.text.toString().plus("4")
-        }
-
-        val button5 = findViewById<Button>(R.id.btNumber5)
-        val textView = findViewById<TextView>(R.id.tvNumber)
-
-        button5.setOnClickListener {
-            textView.text = textView.text.toString().plus("5")
-        }
-
-        val button6 = findViewById<Button>(R.id.btNumber6)
-        val textView = findViewById<TextView>(R.id.tvNumber)
-
-        button6.setOnClickListener {
-            textView.text = textView.text.toString().plus("6")
-        }
-
-        val button = findViewById<Button>(R.id.btNumber7)
-        val textView = findViewById<TextView>(R.id.tvNumber)
-
-        button.setOnClickListener {
-            textView.text = textView.text.toString().plus("7")
-        }
-
-        val button8 = findViewById<Button>(R.id.btNumber8)
-        val textView = findViewById<TextView>(R.id.tvNumber)
-
-        button8.setOnClickListener {
-            textView.text = textView.text.toString().plus("8")
-        }
-
-        val button9 = findViewById<Button>(R.id.btNumber9)
-        val textView = findViewById<TextView>(R.id.tvNumber)
-
-        button9.setOnClickListener {
-            textView.text = textView.text.toString().plus("9")
-        }
-
-        val btpoint = findViewById<Button>(R.id.btPoint)
-        btpoint.setOnClickListener {
-            findViewById<TextView>(R.id.tvNumber).setText(".")
+            if (tvString.isNotEmpty()) {
+                tvCalculator.text = tvString.plus(".")
+            }
 
         }
+        btComma.setOnClickListener {
+            val tvString = tvCalculator.text.toString()
 
-        val btcomma = findViewById<Button>(R.id.btComma)
-
-        btcomma.setOnClickListener {
-            findViewById<TextView>(R.id.tvNumber).setText(",")
+            if (tvString.isNotEmpty()) {
+                tvCalculator.text = tvString.plus(",")
+            }
         }
 
 
-        val btminus = findViewById<Button>(R.id.btMinus)
-        val btdivide = findViewById<Button>(R.id.btDivision)
-        val btmultiply = findViewById<Button>(R.id.btMultiply)
-        val btmodule = findViewById<Button>(R.id.btModule)
-        val btequal = findViewById<Button>(R.id.btEqual)
-
-        val btplus = findViewById<Button>(R.id.btPlus)
-
-        val btAC = findViewById<Button>(R.id.btAc)
-        btAC.setOnClickListener {
-            findViewById<TextView>(R.id.tvNumber).setText("")
-        }
-
-        val btErase = findViewById<Button>(R.id.btErase)
-
-
-
-        }
-        }
+    }
+}
 
 
 
